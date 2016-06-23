2efc1d5bab52b6337520c03d1b83292f7483d1e9
caesar <newman0934@gmail.com>
2016-06-23 19:10:47 +0800

    test3
    test3

---
+++ src/gamebuy/gb/test/TestCustomer.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamebuy.gb.test;
+
+import gamebuy.gb.domain.Customer;
+import gamebuy.gb.domain.GameBuyException;
+import java.text.DateFormat;
+import java.util.GregorianCalendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Administrator
+ */
+public class TestCustomer {
+    public static void main(String[] args) {
+        try {
+            Customer customer = new Customer();
+            customer.setId("a123456789");
+            customer.setBirthday("abcd/12/12");
+            System.out.println(customer);
+        } catch (GameBuyException ex) {
+            Logger.getLogger(TestCustomer.class.getName()).log(Level.SEVERE, null, ex);
+            if(ex.getCause()!=null){
+                System.out.println("..."+ex.getCause());
+            }
+        }
+        
+    }
+}
